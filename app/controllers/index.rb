f41ef3379d8e367dd9631e385f42d8ebdcc350f0
Dan Hossom <dan@Dans-MacBook-Pro-2.local>
2013-08-08 23:05:51 -0700

    route skeletons written, migration written, seed file written

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,48 @@
+
+# Home page
 get '/' do
   # Look in app/views/index.erb
   erb :index
+end
+
+# Display list of notes
+get '/list' do
+
+  erb :list
+end
+
+# gets form for new note
+get '/new_note' do
+
+  erb :new
+end
+
+# write new note to DB
+post '/write_note' do
+
+  erb :list  
+end
+
+# view one note
+get '/notes/:id' do
+
+  erb :show
+end
+
+# edit one note
+get '/notes/:id/edit' do
+
+  erb :edit
+end
+
+# make changes to an existing note
+put '/notes/:id/update' do
+
+  redirect "/notes/#{@note.id}"
+end
+
+# delete a note
+delete '/notes/:id/trash' do
+
+  redirect '/notes'
 end
---
+++ db/seeds.rb
@@ -0,0 +1,6 @@
+Note.create(title: "MAIN TODO", content: "finish this fucking solo, once and for all.")
+Note.create(title: "Next...", content: "...Become JavaScript BAU5.")
+Note.create(title: "Next...", content: "...Become JQuery BAU5.")
+Note.create(title: "Next...", content: "...Become CSS BAU5.")
+Note.create(title: "Finally:", content: "Challenge Sherif to motorcycle pool-cue jousting")
+
